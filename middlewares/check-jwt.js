9a6df230283dea15ad704316b4ca5429d60d2567
Samita <sn.natte@gmail.com>
2020-05-06 00:01:00 +0700

    update jwt

--- middlewares/check-jwt.js
+++ middlewares/check-jwt.js
@@ -4,27 +4,40 @@ require('dotenv').config();
 var secret = process.env.SECRET;
 
 module.exports = function(req, res, next) {
-  let token = req.headers["authorization"];
-
+  let authorization = req.headers['authorization'];
+  let token = req.headers["authorization"].split(' ')[1];
+  console.log(token);
+  if(authorization===undefined) 
+  {console.log(`1errrrrrrrr`);
+  return res.status(401).json({
+    "status": 401,
+    "message": "Unauthorized"
+  
+});
+}
   if (token) {
-    console.log(`errrrrrrrr`);
+    console.log(`2errrrrrrrr`);
     jwt.verify(token, secret, function(err, decoded) {
       if (err) {
-        console.log(`errrrrr`);
-        res.json({
+        console.log(`errrrrrrrr`);
+        return res.status(401).json({
           success: false,
           message: 'Failed to authenticate token'
         });
-      } else {
-
+      } 
+      if(decoded.role === undefined || decoded.role !== 'admin'){
+        return res.status(403).json({
+          success: false,
+          message: 'Forbidden'
+      });   
+      }
+      else {
         req.decoded = decoded;
         next();
-
       }
     });
 
   } else {
-
     res.status(403).json({
       success: false,
       message: 'No token provided'
--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router  = express.Router();
 const con = require('../config/database');
 const jwt = require('jsonwebtoken');
-//const checkJWT = require('../middlewares/check-jwt');
+const checkJWT = require('../middlewares/check-jwt');
 //const upload = require('../middlewares/uploadMiddleware');
 const userController = require('../controller/userController');
 
@@ -12,12 +12,12 @@ require('dotenv').config();
 
 //router.post('/user/login', userController.login);
 
-router.get('/user/getUsers',  userController.getUsers);
+router.get('/user/getUsers', checkJWT, userController.getUsers);
 
-router.get('/user/getUser/:id', userController.getSingleUser);
+router.get('/user/getUser/:id', checkJWT, userController.getSingleUser);
 
-router.post('/user/updateUser/:id', userController.updateUserAccount);
+router.post('/user/updateUser/:id', checkJWT, userController.updateUserAccount);
 
-router.delete('/user/deleteUser/:id', userController.deleteUserAccount);
+router.delete('/user/deleteUser/:id', checkJWT, userController.deleteUserAccount);
 
 module.exports = router;
