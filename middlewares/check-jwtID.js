0204429d3529e473d28f2e3f3c1e2bc22841c9f5
Samita <sn.natte@gmail.com>
2020-05-07 02:38:08 +0700

    Update check-jwtID.js

--- middlewares/check-jwtID.js
+++ middlewares/check-jwtID.js
@@ -32,6 +32,7 @@ module.exports = function(req, res, next) {
       console.log(`decoded role: %s`, decoded.role);
       console.log(`decoded id: %d`, decoded.user_id);
       console.log(`decoded id: %d`,req.params.id);
+      console.log(decoded.user_id === req.params.id);
       if(decoded.user_id === req.params.id){
         req.decoded = decoded;
         next();
