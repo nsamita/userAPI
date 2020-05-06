b0b07de4a6e67dc4b9e0b3f9648942b217c17f0e
Samita <sn.natte@gmail.com>
2020-05-06 19:13:02 +0700

    update .env and checkjwtUser

--- middlewares/check-jwtUser.js
+++ middlewares/check-jwtUser.js
@@ -30,15 +30,15 @@ module.exports = function(req, res, next) {
         });
       } 
       console.log(`decoded: %s`, decoded.role);
-      if(decoded.role === undefined || decoded.id !== req.params.id || decoded.id === undefined){
-        return res.status(403).json({
-          success: false,
-          message: 'Forbidden'
-      });   
-      }
-      else {
+      if((decoded.role !== undefined && decoded.id === req.params.id && decoded.id !== undefined) || (decoded.role === 'admin' )){
         req.decoded = decoded;
         next();
+      }
+      else {
+        return res.status(403).json({
+            success: false,
+            message: 'Forbidden'
+        });  
       }
     });
   } else {
