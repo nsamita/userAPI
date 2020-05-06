3b9496843bc448ad17515e845164ca6b1fd0d1fd
Samita <sn.natte@gmail.com>
2020-05-06 22:11:58 +0700

    roll back

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ var router  = express.Router();
 const con = require('../config/database');
 const jwt = require('jsonwebtoken');
 const checkJWT = require('../middlewares/check-jwt');
-const checkJwtUser = require('../middlewares/check-jwtUser');
+//const checkJwtUser = require('../middlewares/check-jwtUser');
 //const upload = require('../middlewares/uploadMiddleware');
 const userController = require('../controller/userController');
 
@@ -13,12 +13,12 @@ router.post('/user/signup', userController.createUser);
 
 //router.post('/user/login', userController.login);
 
-router.get('/user/getUsers', checkJWT, userController.getUsers);
+router.get('/user/getUsers', userController.getUsers);
 
 router.get('/user/getUser/:id', userController.getSingleUser);
 
-router.patch('/user/updateUser/:id', checkJwtUser, userController.updateUser);
+router.patch('/user/updateUser/:id', userController.updateUser);
 
-router.delete('/user/deleteUser/:id', checkJwtUser, userController.deleteUserAccount);
+router.delete('/user/deleteUser/:id',  userController.deleteUserAccount);
 
 module.exports = router;
