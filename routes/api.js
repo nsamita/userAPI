4890a1611c75a4b1d30101fd8189966fd8389cd1
Samita <sn.natte@gmail.com>
2020-05-06 23:17:02 +0700

    add check jwt ID

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ var router  = express.Router();
 const con = require('../config/database');
 const jwt = require('jsonwebtoken');
 const checkJWT = require('../middlewares/check-jwt');
-//const checkJwtUser = require('../middlewares/check-jwtUser');
+const checkJwtID = require('../middlewares/check-jwtID');
 //const upload = require('../middlewares/uploadMiddleware');
 const userController = require('../controller/userController');
 
@@ -15,10 +15,10 @@ router.post('/user/signup', userController.createUser);
 
 router.get('/user/getUsers', checkJWT, userController.getUsers);
 
-router.get('/user/getUser/:id', userController.getSingleUser);
+router.get('/user/getUser/:id',checkJwtID, userController.getSingleUser);
 
-router.patch('/user/updateUser/:id', userController.updateUser);
+router.patch('/user/updateUser/:id', checkJwtID, userController.updateUser);
 
-router.delete('/user/deleteUser/:id',  userController.deleteUserAccount);
+router.delete('/user/deleteUser/:id', checkJwtID, userController.deleteUserAccount);
 
 module.exports = router;
